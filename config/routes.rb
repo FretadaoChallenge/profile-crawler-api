c24af291ce0997393bbccdde6742e3cbfc5027be
ThalissonMelo <tallisonmelo46@gmail.com>
2019-09-24 22:52:59 -0300

    Adds the project routs

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :members
+  get '/search', to: 'searches#search_member', as: 'search'
+  get '/:id', to: 'shortener/shortened_urls#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
