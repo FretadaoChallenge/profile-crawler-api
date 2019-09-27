a0e876c753ac55e65cb4cba29acaed9cdae4e984
ThalissonMelo <tallisonmelo46@gmail.com>
2019-09-27 02:05:19 -0300

    Fixes search method

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -4,7 +4,11 @@ class SearchesController < ApplicationController
     def search_member
         search_result = Member.all.search(params[:search])
 
-        return render json: search_result if !search_result.blank?
+        if search_result.blank?
+            render status: :unprocessable_entity
+        else
+            render json: search_result
+        end
     end
 
 end
